223deaf366d49e403a06e65be0848049e450f7e0
oscar <oll301189@gmail.com>
2020-08-05 18:18:19 -0300

    Crear test2 y modf

---
+++ tests/test2.js
@@ -0,0 +1,9 @@
+fixture `test` 
+    .page `https://devexpress.github.io/testcafe/`;
+
+test('Validar la url y el título de la página 3', async t => {
+    console.log('-----------------------------------------------------------------------------------------');
+   
+  });
+  
+  /*=======================================================================*/
