1005ee2c82a457e469cf46018e0b0b2d0376c61c
oscar <oll301189@gmail.com>
2020-08-05 18:10:58 -0300

    Modif test y page-model

--- pages/page-model.js
+++ pages/page-model.js
@@ -16,60 +16,24 @@ class Page {
 
         this.barraNav = Selector('.doc-top-nav-wrapper');
     }
-    
+
     async guardarTexto (funcion) {
-     //   await t
+   //   await t
         const funcionTexto = funcion
-    //    .child('p')       
+   //    .child('p')       
         const resultFun =  funcionTexto.innerText
         return resultFun;
     }
-   
+
+    async guardarURL (url) {
+    //   await t
+        const funcionURL = url
+        .child('a')
+        .withAttribute('href', 'url');
+               
+       // const resultFun =  funcionURL.innerText
+      //  return url;
+         }
 }
 
-test('Presionar boton de inicio 2', async t => {
-    console.log('-----------------------------------------------------------------------------------------');
-    let guardarFuncion = page.button 
-    await t
-    .click(guardarFuncion)  
-  });
-  
-  /*=======================================================================*/
-
-  test('Validar el título de la página 3', async t => {
-    console.log('-----------------------------------------------------------------------------------------');
-    let actualResult = '';
-    let expectedResultTitle = 'A node.js tool to automate end-to-end web testing';
-    let guardarTitulo = page.titulo          
-   
-    const extraerTItulo = await page.guardarTexto(guardarTitulo);
-    actualResult = extraerTItulo.replace("\n"," "); 
-  
-    if(actualResult == expectedResultTitle ){
-      console.log('La validacion del titulo fue correcta : ' + expectedResultTitle);
-    } else {
-      console.log('La validacion del titulo fue incorrecta : ' + actualResult);
-    }  
-  });
-  
-  /*=======================================================================*/
-
-  test
- .page `https://devexpress.github.io/testcafe/documentation/getting-started/`
- ('Validar Nav 4', async t => {
-  console.log('-----------------------------------------------------------------------------------------');
-  //  await t
-  let actualResult = '';
-  let myArrayNavBar = ["Getting Started", "Guides", "Reference", "RecipesSSS" , "How It Works" , "Examples"];
- 
-  for(let i = 0; i < myArrayNavBar.length; i++){ 
-    let saveNavBar = page.barraNav.child(i);
-    const extraerTItulo = await page.guardarTexto(saveNavBar);
-    actualResult = extraerTItulo.replace("\n"," ");
-    if(actualResult == myArrayNavBar[i]){
-      console.log('Validacion correcta : ' + myArrayNavBar[i]);
-    } else {
-      console.log('Validacion incorrecta : ' + myArrayNavBar[i] + ' distinto de ' + actualResult);
-      }
-    } //https://devexpress.github.io/testcafe/ 
-}); 
+export default new Page();
