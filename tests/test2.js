5634f3b5397b366697c11c4efa648b41cd23dc89
oscar <oll301189@gmail.com>
2020-08-05 18:21:41 -0300

    Modf test2

--- tests/test2.js
+++ tests/test2.js
@@ -1,9 +1,15 @@
+import { Selector } from 'testcafe';
+import page from '../pages/page-model';
+
 fixture `test` 
     .page `https://devexpress.github.io/testcafe/`;
 
-test('Validar la url y el título de la página 3', async t => {
+test('Presionar boton de inicio 1', async t => {
     console.log('-----------------------------------------------------------------------------------------');
-   
-  });
+    let guardarFuncion = page.button 
+    await t
+    .click(guardarFuncion)  
+});
+      
+/*=======================================================================*/
   
-  /*=======================================================================*/
